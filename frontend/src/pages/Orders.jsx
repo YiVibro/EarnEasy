cc5bba2b892e5552f43b0fb456c878b750ef42e4
Yishith Vilas G <149123685+YiVibro@users.noreply.github.com>
2025-07-17 12:05:36 +0530

    Update Orders.jsx

--- frontend/src/pages/Orders.jsx
+++ frontend/src/pages/Orders.jsx
@@ -1,57 +1,14 @@
-// import image from "../assets/order_not_found.png";
-// import { useState, useEffect } from "react";
-// import { ArrowLeft } from "lucide-react";
-// import OrdersCard from "../components/OrdersCard";
-// import ordersdata from "../data/ordersdata.json";
-
-// export default function Orders() {
-//   const [products, setProducts] = useState([]);
-
-//   useEffect(() => {
-//     setProducts(ordersdata);
-//   }, []);
-
-//   return (
-//     <div className="relative flex flex-col h-100%">
-//       <div className="orders-header bg-[#30B0C7] rounded-[15px] h-[80px] relative top-[-40px] flex flex-col justify-end items-center ">
-//         <ArrowLeft id="come_back" className="absolute left-[10px] z-10 top-[48px]" />
-//         <p className="absolute bottom-0 p-4 text-black text-2xl font-bold top-[25px]">
-//           My Orders
-//         </p>
-//       </div>
-
-//       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6 p-4">
-//         {products.length === 0 ? (
-//           <div className="col-span-full flex flex-col justify-center items-center">
-//             <img src={image} alt="No orders found" className="w-64" />
-//             <p>You havent placed any order yet!!</p>
-//           </div>
-//         ) : (
-//           products.map((element, index) => (
-//             <OrdersCard
-//               key={index}
-//               img={element.image}
-//               genre={element.genre}
-//               productName={element.productName}
-//               pricePerHour={element.pricePerHour}
-//               quantity={element.quantity}
-//               orderStatus={element.orderStatus}
-//             />
-//           ))
-//         )}
-//       </div>
-//     </div>
-//   );
-// }
 
 import image from "../assets/order_not_found.png";
 import { useState, useEffect } from "react";
 import { ArrowLeft } from "lucide-react";
 import OrdersCard from "../components/OrdersCard";
 import ordersdata from "../dummyData/ordersdata.json";
+import {useNavigate} from "react-router-dom";
 
 export default function Orders() {
   const [products, setProducts] = useState([]);
+  const navigate=useNavigate();
 
   useEffect(() => {
     setProducts(ordersdata);
@@ -61,7 +18,9 @@ export default function Orders() {
     <div className="min-h-screen px-4 py-6 bg-gray-100 transition-colors duration-300">
       {/* Header */}
       <div className="relative flex items-center mb-6">
-        <ArrowLeft className="text-black mr-4" />
+                 <button onClick={() => navigate('/')}>
+                     <ArrowLeft className="text-black mr-4" />
+                 </button>
         <h1 className="text-2xl font-bold sm:text-center sm:w-full">My Orders</h1>
       </div>
 
